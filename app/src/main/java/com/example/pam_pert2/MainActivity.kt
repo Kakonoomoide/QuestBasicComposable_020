2ec73639e21a2c31a696d0150d9ee7aa05a6f14c
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-18 10:26:27 +0700

    Style - mengubah warna text "Rizki Ramadan" dengan warna costumize

--- app/src/main/java/com/example/pam_pert2/MainActivity.kt
+++ app/src/main/java/com/example/pam_pert2/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -79,7 +80,7 @@ fun BasicCompose(modifier: Modifier = Modifier) {
             text = "Rizki Ramadan",
             style = TextStyle(
                 fontSize = 30.sp,
-                color = Color.DarkGray,
+                color = colorResource(id = R.color.gray_500),
                 fontFamily = FontFamily.Monospace
             )
         )
