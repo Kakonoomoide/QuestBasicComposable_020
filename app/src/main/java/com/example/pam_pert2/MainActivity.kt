afd4aed35f942124ce0c0278aba72d4d1b7141d7
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-18 10:13:45 +0700

    Style - mengubah bentuk foto menjadi lingkaran

--- app/src/main/java/com/example/pam_pert2/MainActivity.kt
+++ app/src/main/java/com/example/pam_pert2/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -88,7 +90,9 @@ fun BasicCompose(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(id = R.drawable.profille_rizki),
             contentDescription = null,
-            modifier = Modifier.size(200.dp)
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
         )
     }
 }
